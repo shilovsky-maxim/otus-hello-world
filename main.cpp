01cf8455d50efa70897a870dfd1eaab18f48672c
shilovsky-maxim <max-in-3d@ngs.ru>
2021-09-26 20:16:30 +0700

    Add build number to output
    * Changes CMakeLists.txt to configure version.h file containing a
    constant with the project build number.
    * Changes main.cpp to output build number before "Hello World"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "version.h"
+
 int main (int, char **)
 {
+    std::cout << "build " << PROJECT_VERSION_PATCH << std::endl;
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
